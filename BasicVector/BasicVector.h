830be2f1092d4622a1914e701bcba4faa4e0899e
Jing Wang <jing@figur8.me>
2019-05-18 12:39:18 -0400

    Init commit

---
+++ BasicVector/BasicVector.h
@@ -0,0 +1,19 @@
+//
+//  BasicVector.h
+//  BasicVector
+//
+//  Created by Jing Wang on 5/18/19.
+//  Copyright © 2019 Jing Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BasicVector.
+FOUNDATION_EXPORT double BasicVectorVersionNumber;
+
+//! Project version string for BasicVector.
+FOUNDATION_EXPORT const unsigned char BasicVectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BasicVector/PublicHeader.h>
+
+
---
+++ BasicVector/BasicVector.swift
@@ -0,0 +1,40 @@
+//
+//  BasicVector.swift
+//  BasicVector
+//
+//  Created by Jing Wang on 5/18/19.
+//  Copyright © 2019 Jing Wang. All rights reserved.
+//
+
+import Foundation
+
+//MARK: Types
+
+public typealias Scalar = Float
+
+//MARK: Scalar
+
+public extension Scalar {
+    //public static let pi = Scalar(Double.pi)
+    static let halfPi = Scalar(Scalar.pi / 2)
+    static let quarterPi = Scalar(Scalar.pi / 4)
+    static let twoPi = Scalar(Scalar.pi * 2)
+    static let degreesPerRadian = 180 / pi
+    static let radiansPerDegree = pi / 180
+    static let epsilon: Scalar = 0.0001
+    
+    static func ~=(lhs: Scalar, rhs: Scalar) -> Bool {
+        return Swift.abs(lhs - rhs) < .epsilon
+    }
+    
+    func degToRad() -> Scalar{
+        return self * Scalar.radiansPerDegree
+    }
+    
+    func radToDeg() -> Scalar {
+        return self * Scalar.degreesPerRadian
+    }
+}
+
+
+
